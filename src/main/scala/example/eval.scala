384380eae6b452a71342e6b6f3e4edcb503038d5
h2oche <h2oche22@gmail.com>
2020-12-27 17:42:57 +0900

    [#2] add stateful

--- src/main/scala/example/eval.scala
+++ src/main/scala/example/eval.scala
@@ -5,19 +5,36 @@ object Evaluator {
   case class Const(i: Int) extends Term
   case class Div(t0: Term, t1: Term) extends Term
 
-  type Ex = String
-  trait Result[+T]
-  case class Raise(e: Ex) extends Result[Nothing]
-  case class Return[T](r: T) extends Result[T]
+  ////////////////////////////////////////////////////////////////////////////////
+  // Variation1 : Excpetion
+  ////////////////////////////////////////////////////////////////////////////////
+  // type Ex = String
+  // trait Result[+T]
+  // case class Raise(e: Ex) extends Result[Nothing]
+  // case class Return[T](r: T) extends Result[T]
 
+  // def eval(t: Term): Result[Int] = t match {
+  //   case Const(i) => Return(i)
+  //   case Div(t0, t1) => eval(t0) match {
+  //     case Raise(e) => Raise(e)
+  //     case Return(r0) => eval(t1) match {
+  //       case Raise(e) => Raise(e)
+  //       case Return(r1) => if ( r1 == 0) Raise("divide by zero") else Return(r0 / r1)
+  //     }
+  //   }
+  // }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  // Variation2 : State (which means # of division during evaluation)
+  ////////////////////////////////////////////////////////////////////////////////
+  type State = Int
+  type Result[T] = State => (T, State)
   def eval(t: Term): Result[Int] = t match {
-    case Const(i) => Return(i)
-    case Div(t0, t1) => eval(t0) match {
-      case Raise(e) => Raise(e)
-      case Return(r0) => eval(t1) match {
-        case Raise(e) => Raise(e)
-        case Return(r1) => if (r1 == 0) Raise("divide by zero") else Return(r0 / r1)
-      }
+    case Const(i) => (st: State) => (i, st)
+    case Div(t0, t1) => (st: State) => {
+      val (r0, st0) = eval(t0)(st)
+      val (r1, st1) = eval(t1)(st0)
+      (r0 / r1, st1 + 1)
     }
   }
 }
--- src/main/scala/example/main.scala
+++ src/main/scala/example/main.scala
@@ -5,6 +5,8 @@ import example.Evaluator._
 object Main extends App {
   val ans = Div(Div(Const(1972), Const(2)), Const(23))
   val err = Div(Const(1), Const(0))
-  println(eval(ans))
-  println(eval(err))
+  // println(eval(ans))
+  // println(eval(err))
+  println(eval(ans)(0))
+  println(eval(err)(0))
 }
