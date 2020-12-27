3786f7952786110e794dc1eb17429eea9d1b4103
h2oche <h2oche22@gmail.com>
2020-12-27 17:56:03 +0900

    [#2] add output

--- src/main/scala/example/eval.scala
+++ src/main/scala/example/eval.scala
@@ -27,14 +27,31 @@ object Evaluator {
   ////////////////////////////////////////////////////////////////////////////////
   // Variation2 : State (which means # of division during evaluation)
   ////////////////////////////////////////////////////////////////////////////////
-  type State = Int
-  type Result[T] = State => (T, State)
+  // type State = Int
+  // type Result[T] = State => (T, State)
+  // def eval(t: Term): Result[Int] = t match {
+  //   case Const(i) => (st: State) => (i, st)
+  //   case Div(t0, t1) => (st: State) => {
+  //     val (r0, st0) = eval(t0)(st)
+  //     val (r1, st1) = eval(t1)(st0)
+  //     (r0 / r1, st1 + 1)
+  //   }
+  // }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  // Variation3 : Output
+  ////////////////////////////////////////////////////////////////////////////////
+  type Output = String
+  type Result[T] = (T, Output)
+  def line(t: Term, r: Int): String = f"eval($t) => $r\n"
   def eval(t: Term): Result[Int] = t match {
-    case Const(i) => (st: State) => (i, st)
-    case Div(t0, t1) => (st: State) => {
-      val (r0, st0) = eval(t0)(st)
-      val (r1, st1) = eval(t1)(st0)
-      (r0 / r1, st1 + 1)
+    case Const(i) => (i, line(t, i))
+    case Div(t0, t1) => {
+      val (r0, out0) = eval(t0)
+      val (r1, out1) = eval(t1)
+      val r = r0 / r1
+      (r, out0 + out1 + line(t, r))
     }
   }
+
 }
